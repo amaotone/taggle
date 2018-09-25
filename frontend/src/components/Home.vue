6ac46bb54daa0f143016ce5979e6523d97ac77a2
Amane Suzuki <amane.suzu@gmail.com>
2018-09-26 03:09:32 +0900

    Add frontend

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,50 @@
+<template>
+<section class="container">
+    <h1>Taggle</h1>
+    <p>Classify Kaggle competitions by tag.</p>
+    <span v-if="loading">now loading...</span>
+    <table class="table table-hover" v-if="!loading">
+        <thead>
+            <tr>
+                <th scope="col"></th>
+                <th scope="col">Name</th>
+                <th scope="col">Tags</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="compe in competitions" :key="compe.id">
+                <th scope="row"><img :src="compe.image" alt="" width=32 height=32></th>
+                <td><a :href="compe.url">{{compe.name}}</a></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</section>
+</template>
+
+<script>
+import db from './firebaseInit'
+export default {
+  name: 'home',
+  data () {
+    return {
+      competitions: [],
+      loading: true
+    }
+  },
+  created () {
+    db.collection('competitions').get().then((snapshot) => {
+      this.loading = false
+      snapshot.forEach((doc) => {
+        let data = {
+          'id': doc.id,
+          'name': doc.data().name,
+          'url': 'http://www.kaggle.com' + doc.data().url,
+          'image': doc.data().image
+        }
+        this.competitions.push(data)
+      })
+    })
+  }
+}
+</script>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+
+Vue.use(Router)
+Vue.use(BootstrapVue)
+
+export default new Router({
+  routes: [{
+    path: '/home',
+    name: 'Home',
+    component: Home
+  }]
+})
