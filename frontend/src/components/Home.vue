379e724354fe794b17fe3dffa97043ff68bf90d3
Amane Suzuki <amane.suzu@gmail.com>
2018-09-26 23:30:39 +0900

    Enable multi-tag search

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,28 +1,21 @@
 <template>
 <section class="container">
-  <div class="jumbotron">
-    <h1 class="display-5">Taggle</h1>
-    <p class="lead">Find Kaggle competitions with various tags.</p>
-    <input type="text" class="form-control" placeholder="search">
+  <div class="my-4">
+    <h1>Taggle</h1>
+    <p>Find Kaggle competitions with various tags.</p>
+    <input v-model="query" type="text" class="form-control" placeholder="search">
   </div>
   <span v-if="loading">now loading...</span>
-  <table class="table" v-if="!loading">
-    <thead>
-      <tr>
-        <th scope="col"></th>
-        <th scope="col">Name</th>
-        <th scope="col">Tags</th>
-      </tr>
-    </thead>
+  <table class="table table-sm" v-if="!loading">
     <tbody>
-      <tr v-for="compe in competitions.sort((a, b) => b.startdate-a.startdate)" :key="compe.id">
-        <th class="align-middle" scope="row"><img class="rounded" :src="compe.image" alt="" width=60 height=60></th>
-        <td class="align-middle">
+      <tr class="row" v-for="compe in filteredCompetitions" :key="compe.id">
+        <td class="col-5">
+          <img class="rounded float-left m-2" :src="compe.image" alt="" width=60 height=60>
           <a :href="compe.url">{{compe.name}}</a>
           <br>
-          from {{compe.startdate.format('YYYY-MM-DD')}} to {{compe.enddate.format('YYYY-MM-DD')}}
+          {{compe.startdate.format('YYYY-MM-DD')}} / {{compe.enddate.format('YYYY-MM-DD')}}
         </td>
-        <td class="align-middle"><span v-for="tag in Object.keys(compe.tags)" :key="tag" class="badge badge-dark m-1">{{tag}}</span></td>
+        <td class="col-7"><span v-for="tag in compe.tags.split(',')" :key="tag" class="badge badge-dark m-1">{{tag}}</span></td>
       </tr>
     </tbody>
   </table>
@@ -36,8 +29,20 @@ export default {
   name: 'home',
   data () {
     return {
+      query: '',
       competitions: [],
       loading: true
+    }
+  },
+  computed: {
+    filteredCompetitions: function () {
+      if (!this.query) return this.competitions
+      const regs = this.query.split(',').map(q => {
+        return new RegExp(q.trim())
+      })
+      return this.competitions.filter(compe => {
+        return regs.every(reg => reg.test(compe.tags))
+      })
     }
   },
   created () {
@@ -51,10 +56,11 @@ export default {
           'startdate': moment(doc.data().start),
           'enddate': moment(doc.data().end),
           'image': doc.data().image,
-          'tags': doc.data().tags
+          'tags': Object.keys(doc.data().tags).join(',')
         }
         this.competitions.push(data)
       })
+      this.competitions.sort((a, b) => b.startdate - a.startdate)
     })
   }
 }
