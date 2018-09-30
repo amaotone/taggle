df3f07dbafbe6c33c350543ebb9bb1c2c96eba84
Amane Suzuki <amane.suzu@gmail.com>
2018-09-30 23:17:05 +0900

    Install dotenv

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -5,18 +5,32 @@
     <p>Find Kaggle competitions with various tags.</p>
     <input v-model="query" type="text" class="form-control" placeholder="Search">
   </div>
-  <span v-if="loading">now loading...</span>
-  <div class="list-group list-group-flush" v-if="!loading">
-    <button class="list-group-item list-group-item-action justify-content-between" v-for="compe in filteredCompetitions" :key="compe.id">
+  <span v-if="loading">Now Loading...</span>
+  <div class="list-group" v-if="!loading">
+    <button class="list-group-item list-group-item-action" v-for="compe in filteredCompetitions" :key="compe.id" v-on:click="compe.showLinks = !compe.showLinks">
       <div class="row">
-        <div class="col-6">
-          <img class="rounded float-left mr-2" :src="compe.image" alt="" width=60 height=60>
-          <p><a :href="compe.url">{{compe.name}}</a><br><small>{{compe.startdate.format('YYYY-MM-DD')}} / {{compe.enddate.format('YYYY-MM-DD')}}</small></p>
+        <div class="col-6 d-flex justify-content-start align-items-start">
+          <div class="align-self-start mr-2"><img class="rounded" :src="compe.image" alt="" width=60 height=60></div>
+          <div class="align-self-start"><p><a :href="compe.url">{{compe.name}}</a><br><small>{{compe.startdate.format('YYYY-MM-DD')}} / {{compe.enddate.format('YYYY-MM-DD')}}</small></p></div>
         </div>
         <div class="col-5">
-          <p><span v-for="tag in compe.tags.split(',')" :key="tag" class="badge badge-secondary m-1">{{tag}}</span></p>
+          <p><span v-for="tag in compe.tags" :key="tag" class="badge badge-dark mr-2">{{tag}}</span><a href="#"><ion-icon name="create"></ion-icon></a></p>
         </div>
-        <div class="col-1 text-right d-flex align-items-center justify-content-end"><span class="badge badge-light badge-pill">14</span></div>
+        <div class="col-1 text-right d-flex align-items-center justify-content-end">
+          <span class="badge badge-light badge-pill">0</span>
+          </div>
+      </div>
+      <div v-if="compe.showLinks">
+        <ul class="list-group pt-2">
+          <li class="list-group-item">
+            <a href="#">1st place solution</a>
+            <span class="badge badge-dark mx-1">table</span>
+          </li>
+          <li class="list-group-item">
+            <a href="#">1st place solution</a>
+            <span class="badge badge-dark mx-1">table</span>
+          </li>
+        </ul>
       </div>
     </button>
   </div>
@@ -42,7 +56,7 @@ export default {
         return new RegExp(q.trim(), 'i')
       })
       return this.competitions.filter(compe => {
-        return regs.every(reg => reg.test(compe.tags) || reg.test(compe.name))
+        return regs.every(reg => reg.test(compe.searchTarget))
       })
     }
   },
@@ -57,8 +71,10 @@ export default {
           'startdate': moment(doc.data().start),
           'enddate': moment(doc.data().end),
           'image': doc.data().image,
-          'tags': Object.keys(doc.data().tags).join(',')
+          'tags': Object.keys(doc.data().tags),
+          'showLinks': false
         }
+        data['searchTarget'] = data['name'] + ' ' + data['tags'].join(' ')
         this.competitions.push(data)
       })
       this.competitions.sort((a, b) => b.startdate - a.startdate)
