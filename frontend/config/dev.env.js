5a6ad9b4f71a2773b10d4f27617fd05c52ec2b2f
Amane Suzuki <amane.suzu@gmail.com>
2018-10-03 23:58:32 +0900

    Enable dotenv

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -1,9 +1,7 @@
 'use strict'
-require('dotenv').config()
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY)
+  NODE_ENV: '"development"'
 })
--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,4 +1,7 @@
 'use strict'
+require('dotenv').config()
+
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY)
 }
